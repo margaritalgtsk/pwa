4cee23e49260f95a14fccb159293a10e15a3e7f4
margarita <mlogatskaya@scncoft.com>
2026-02-11 12:03:23 +0300

    Initial setup

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA, type ManifestOptions } from "vite-plugin-pwa";
+
+const manifest: false | Partial<ManifestOptions> | undefined = {
+  theme_color: "#8936FF",
+  background_color: "#2EC6FE",
+  icons: [
+    {
+      purpose: "maskable",
+      sizes: "512x512",
+      src: "icon512_maskable.png",
+      type: "image/png",
+    },
+    {
+      purpose: "any",
+      sizes: "512x512",
+      src: "icon512_rounded.png",
+      type: "image/png",
+    },
+  ],
+  screenshots: [
+    {
+      src: "screenshot/desktop.png",
+      sizes: "3308x1586",
+      type: "image/png",
+      form_factor: "wide",
+    },
+    {
+      src: "screenshot/mobile.png",
+      sizes: "698x1096",
+      type: "image/png",
+      form_factor: "narrow",
+    },
+  ],
+  orientation: "any",
+  display: "standalone",
+  //dir: "auto",
+  lang: "ru-RU",
+  name: "pwa",
+  short_name: "pwa",
+};
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({ registerType: "autoUpdate", manifest })],
+});
